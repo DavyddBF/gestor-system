47be27bcab0ed6f0e81ec4a8f554a8ce00a66c89
DavyddBF <davydextreme460@gmail.com>
2024-07-29 15:13:11 -0300

    Mudando onde é chamado o header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import { BrowserRouter } from 'react-router-dom';
 import RouterApp from './router';
 import './App.css';
 
-import Header from './components/Header';
-
 function App() {
   return (
     <BrowserRouter>
-      <Header/>
       <RouterApp/>
     </BrowserRouter>
   )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,9 @@
+import Header from "../../components/Header";
+
 function Home() {
   return (
     <div>
+      <Header/>
       <h1>Olá Mundo</h1>
     </div>
   );  
