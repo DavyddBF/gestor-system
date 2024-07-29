29c3dab17ef7fe23d9157776dbc68444baa2ea87
DavyddBF <davydextreme460@gmail.com>
2024-07-29 15:38:25 -0300

    Modificando Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,19 @@
 import Header from "../../components/Header";
+import Nav from "../../components/Nav";
+
+import './home.css';
 
 function Home() {
   return (
-    <div>
+    <>
       <Header/>
-      <h1>Olá Mundo</h1>
-    </div>
+      <main className="main-container">
+        <div className="main-div">
+          <Nav/>
+          <h1>Olá Mundo</h1>
+        </div>
+      </main>
+    </>
   );  
 }
 
