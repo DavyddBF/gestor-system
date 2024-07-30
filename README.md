efcbfd7a72d8069bc70f2ebf295df8f0fc3d0a9d
DavyddBF <davydextreme460@gmail.com>
2024-07-29 21:40:52 -0300

    Removendo arquivos

--- README.md
+++
@@ -1,5 +0,0 @@
-# Projeto pessoal sobre gestão de serviços
-
-Iniciando projeto com o objetivo de organizar os serviços, por enquanto focado em serviços de grama e pintura.
-
-Ideia pensada para ajudar meu pai a conseguir organizar os serviços que ele tem para fazer.
--- sistema/src/pages/Clientes/index.jsx
+++ sistema/src/pages/Clientes/index.jsx
@@ -9,7 +9,29 @@ function Clientes() {
       <main className="main-container">
         <div className="main-div">
           <Nav/>
-          <div>TODOS OS CLIENTES AQUI</div>
+        
+        
+          <div>
+            <h2>Cortes de Grama</h2>
+            <div>
+              <input type="text" />
+              <button>Buscar</button>
+            </div>
+            <div>
+              <div>
+                <strong>Davyd Ferreira</strong>
+                <p>Ultimo serviço: <span>29/07</span></p>
+                <p>Próximo serviço: <span>12/08</span></p>
+                <p>Valor cobrado: <span>100,00</span></p>
+
+                <button>Feito!!</button>
+                <button>Editar</button>
+              </div>
+            </div>
+          </div>
+
+
+
         </div>
       </main>
     </>
