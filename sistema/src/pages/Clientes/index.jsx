850fd30471fcb103f45c44f4a0f77f39922ee8ea
DavyddBF <davydextreme460@gmail.com>
2024-07-30 14:06:03 -0300

    Criando lógica de mostrar os clientes

--- sistema/src/pages/Clientes/index.jsx
+++ sistema/src/pages/Clientes/index.jsx
@@ -1,9 +1,64 @@
+import { useEffect, useState } from 'react';
+import { collection, getDocs, onSnapshot } from 'firebase/firestore';
+import { db } from '../../firebase';
+import iconPessoa from '../../assets/pessoa-icon.svg';
 import './clientes.css';
 
 import Header from "../../components/Header";
 import Nav from "../../components/Nav";
 
 function Clientes() {
+  const [clientes, setClientes] = useState([]);
+
+  useEffect(() => {
+    async function carregaClientes() {
+      const refDoc = collection(db, 'clientes');
+      const unsub = onSnapshot(refDoc, snapshot => {
+        let listaClientes = [];
+
+        snapshot.forEach( cliente => {
+          listaClientes.push({
+            id: cliente.id,
+            nome: cliente.data().nome,
+            telefone: cliente.data().telefone,
+            ultimaData: cliente.data().ultimaData,
+            proxData: cliente.data().proxData,
+            valor: cliente.data().valor
+          });
+        });
+
+        setClientes(listaClientes);
+      });
+
+      return () => unsub();
+    }
+
+    carregaClientes();
+  }, []);
+
+  async function buscarTodosClientes() {
+    const refDoc = collection(db, 'clientes');
+    await getDocs(refDoc)
+    .then( snapshot => {
+      let listaClientes = [];
+
+      snapshot.forEach( cliente => {
+        listaClientes.push({
+          id: cliente.id,
+          nome: cliente.data().nome,
+          telefone: cliente.data().telefone,
+          ultimaData: cliente.data().ultimaData,
+          proxData: cliente.data().proxData,
+          valor: cliente.data().valor
+        });
+      });
+
+      setClientes(listaClientes);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
 
   return (
     <>
@@ -12,28 +67,43 @@ function Clientes() {
         <div className="main-div">
           <Nav/>
         
-        
           <div className='clientes-container'>
             <div className='clientes-campo'>
-              <input className="clientes-pesquisa" type="text" />
+              <input className="clientes-pesquisa" type="text" placeholder='Pesquisar...' />
               <button className='clientes-pesquisa--btn'>Buscar</button>
+              <button className='clientes-pesquisa--btn' onClick={ buscarTodosClientes }>Atualizar</button>
             </div>
             <div className='cliente-container'>
-              <div className='cliente'>
-                <strong className='cliente-nome'>Davyd Ferreira</strong>
-                <button className='cliente-info--btn'>Infos</button>
-                <p className='cliente-ultimo--servico'>Ultimo serviço: <span>29/07</span></p>
-                <p className='cliete-prox--servico'>Próximo serviço: <span>12/08</span></p>
-                <p className='cliente-valor'>Valor cobrado: <span>100,00</span></p>
-
-                <button className='cliente-btn--feito'>Feito!!</button>
-                <button className='cliente-btn--editar'>Editar</button>
-              </div>
+              {
+                clientes.map( cliente => {
+                  return (
+                    
+                      <div className='cliente' key={ cliente.id }>
+                        <div>
+                          <div>
+                            <img className='cliente-img' src={ iconPessoa } alt="Icone de Perfil" />
+                            <strong className='cliente-nome'>{ cliente.nome }</strong>
+                          </div>
+                          <button className='cliente-info--btn'>Infos</button>
+                        </div>
+                        <div>
+                          <p className='cliente-ultimo--servico'>Ultimo serviço: <span>{ cliente.ultimaData }</span></p>
+                          <p className='cliente-prox--servico'>Próximo serviço: <span>{ cliente.proxData }</span></p>
+                        </div>
+                        
+                        <div>
+                          <p className='cliente-valor'>Valor cobrado: <span>{ cliente.valor }</span></p>
+                          <div>
+                            <button className='cliente-btn--feito'>Feito!!</button>
+                            <button className='cliente-btn--editar'>Editar</button>
+                          </div>
+                        </div>
+                      </div>
+                  );
+                })
+              }
             </div>
           </div>
-
-
-
         </div>
       </main>
     </>
