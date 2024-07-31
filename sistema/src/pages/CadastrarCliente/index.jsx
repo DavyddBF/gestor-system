76205fbcf31f1615cf226fba40247de73be6021f
DavyddBF <davydextreme460@gmail.com>
2024-07-31 16:58:09 -0300

    Anterior addCliente, com o nome de pasta novo

---
+++ sistema/src/pages/CadastrarCliente/index.jsx
@@ -0,0 +1,21 @@
+import './cadastrarCliente.css';
+
+import Header from '../../components/Header';
+import Nav from '../../components/Nav';
+
+function CadastrarCliente() {
+  return (
+    <>
+      <Header/>
+      <main className="main-container">
+        <div className="main-div">
+          <Nav/>
+          
+          
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default CadastrarCliente;
