35d6fb712f96991b5655490846b69e2ab1136303
DavyddBF <davydextreme460@gmail.com>
2024-07-31 16:58:42 -0300

    Criando nova rota

--- sistema/src/router.jsx
+++ sistema/src/router.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Clientes from './pages/Clientes';
+import CadastrarCliente from './pages/CadastrarCliente';
 
 function RouterApp() {
   return (
@@ -10,6 +11,7 @@ function RouterApp() {
       <Route path='/' element={ <Home/> } />
       <Route path='/dashboard' element={ <Dashboard/> } />
       <Route path='/dashboard/clientes' element={ <Clientes/> } />
+      <Route path='/dashboard/clientes/cadastrar' element={ <CadastrarCliente /> } />
     </Routes>
   );
 }
